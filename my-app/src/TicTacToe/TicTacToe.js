772d8bd86be5deb702ee7c58d4c2235300310faf
parthiv4698patel <78160490+parthiv4698patel@users.noreply.github.com>
2022-08-24 15:36:46 -0400

    pass the number
    verify each cell to pass the number in alert pop up.

--- my-app/src/TicTacToe/TicTacToe.js
+++ my-app/src/TicTacToe/TicTacToe.js
@@ -2,12 +2,12 @@ import React from "react";
 import "./TicTacToe.css";
 
 const TicTacToe = () => {
-  const handleClick = () => {
-    alert("test");
+  const handleClick = (num) => {
+    alert(num);
   };
 
   const Cell = ({ num }) => {
-    return <td onClick={() => handleClick()}></td>;
+    return <td onClick={() => handleClick(num)}>parthiv</td>;
   };
 
   return (
@@ -15,19 +15,19 @@ const TicTacToe = () => {
       <table>
         <tbody>
           <tr>
-            <Cell></Cell>
-            <Cell></Cell>
-            <Cell></Cell>
+            <Cell num={0} />
+            <Cell num={1} />
+            <Cell num={2} />
           </tr>
           <tr>
-            <Cell></Cell>
-            <Cell></Cell>
-            <Cell></Cell>
+            <Cell num={3} />
+            <Cell num={4} />
+            <Cell num={5} />
           </tr>
           <tr>
-            <Cell></Cell>
-            <Cell></Cell>
-            <Cell></Cell>
+            <Cell num={6} />
+            <Cell num={7} />
+            <Cell num={8} />
           </tr>
         </tbody>
       </table>
